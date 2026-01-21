464c6a7bba102406e258073f3e11083561917c9e
ynsz <99311398+ynsz@users.noreply.github.com>
2026-01-21 23:04:45 +0900

    ユーザー登録機能の実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sns_app/screens/home_screen.dart';
 import 'package:sns_app/screens/signin_screen.dart';
 import 'package:sns_app/screens/signup_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.env["SUPABASE_URL"]!,
+    anonKey: dotenv.env["SUPABASE_API_KEY"]!,
+  );
   runApp(const SnsApp());
 }
 
---
+++ lib/modules/auth/auth.repository.dart
@@ -0,0 +1,12 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthRepository {
+  Future<User?> signup(String name, String email, String password) async {
+    final response = await Supabase.instance.client.auth.signUp(
+      email: email,
+      password: password,
+      data: {"name": name},
+    );
+    return response.user;
+  }
+}
